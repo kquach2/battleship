7bb1111690a22533fcf865e9dd3677edbb7ff0f2
Kevin Quach <kevinquach2@yahoo.com>
2022-07-31 17:11:12 -0500

    Webpack installed

--- app-logic.test.js
+++ app-logic.test.js
@@ -1,4 +1,4 @@
-import shipFactory from "./app-logic";
+import shipFactory from "./src/app-logic";
 
 test("getLength returns the correct length of a ship", () => {
   expect(shipFactory(3).getLength()).toBe(3);
